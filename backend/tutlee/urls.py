d1054ce7cc48b5ddc4ebeb1133dbc2ae4d215892
Techmissions <ebube@tutlee.com>
2026-08-04 18:12:35 +0100

    Update urls.py

--- backend/tutlee/urls.py
+++ backend/tutlee/urls.py
@@ -77,6 +77,7 @@ urlpatterns = [
     path('help.html',     TemplateView.as_view(template_name='help.html'),     name='help'),
     path('privacy.html',  TemplateView.as_view(template_name='privacy.html'),  name='privacy'),
     path('partners.html',    TemplateView.as_view(template_name='partners.html'),    name='partners'),
+    path('press.html',    TemplateView.as_view(template_name='press.html'),    name='press'),
     path('research.html', TemplateView.as_view(template_name='research.html'), name='research'),
     path('safety.html',   TemplateView.as_view(template_name='safety.html'),   name='safety'),
     path('terms.html',    TemplateView.as_view(template_name='terms.html'),    name='terms'),
